7d48618aa26292135ccab81af59212236a32c13a
Jacob Kristensen <31685155+jacobkri@users.noreply.github.com>
2019-07-09 14:51:27 +0200

    Updated comments

--- lib/core_classes/core_helpers_class.php
+++ lib/core_classes/core_helpers_class.php
@@ -1,10 +1,10 @@
 <?php
-
-// 2018 was the year it suddenly became clear that we needed a "core" type of class
-// for helper methods
-
-// Keep this class independent from others, so that it may be used outside of the main Doorkeeper code
-// Methods in this class are "helpers" to solve common coding problems
+/*
+            Doorkeeper
+            
+              Independent class containing helper methods used to solve common programming problems
+*/
+            
 class core_helpers
 {
 
